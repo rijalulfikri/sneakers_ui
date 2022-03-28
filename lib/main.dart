1c25f97067887a7233b40b8e53abe3c611933cfa
Rijalul Fikri <rijalulfikri@gmail.com>
2022-03-28 17:55:22 +0700

    Ubah main title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sneakers Shop',
+      title: 'Sneakers OLShop',
       home: ProductCatalogue(),
     );
   }
